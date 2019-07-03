6ce1d4c520e2552cc5273c20e8b7ece6d22ea989
Thanh <tmdoan98@gmail.com>
2019-07-03 18:24:27 +0100

    add: header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,21 @@
-import React from "react"
-import styles from "./Header.module.scss"
+import React from 'react'
+import styles from './Header.module.scss'
+import NavBar from '../NavBar'
+import NavItem from '../NavItem'
+import { Location } from '@reach/router'
 
-const Header = ({ children }) => (
-  <header className={styles.Header}>{children}</header>
+const Header = () => (
+  <header className={styles.Header}>
+    <Location>
+      {({ location }) => (
+        <NavBar path={location.pathname}>
+          <NavItem to="/">Home</NavItem>
+          <NavItem to="/portfolio">Portfolio</NavItem>
+          <NavItem to="/contact">Contact</NavItem>
+        </NavBar>
+      )}
+    </Location>
+  </header>
 )
 
 export default Header
